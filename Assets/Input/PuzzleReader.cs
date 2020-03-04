69ff37c7ed30cfeff890e5a0c72eb86f1ec514c7
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-03-03 20:55:00 -0600

    Worked on puzzle input

---
+++ Assets/Input/PuzzleReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PuzzleReader : MonoBehaviour
+{
+
+    private int rows, columns;
+    private List<string> rowSpecs, columnSpecs;
+
+    public PuzzleReader()
+    {
+        rowSpecs = new List<string>();
+        columnSpecs = new List<string>();
+    }
+
+    void Start()
+    {
+        string path = "Assets/Input/puzzle.txt";
+        if (File.Exists(path))
+        {
+            string[] lines = File.ReadAllLines(path);
+            string[] size = lines[0].Split(',');
+
+            this.rows = Int32.Parse(size[0].Trim());
+            this.columns = Int32.Parse(size[1].Trim());
+
+            Debug.Log(this.rows + this.columns);
+        } else Debug.Log("No se encontró el archivo necesario de nivel");
+    }
+}
