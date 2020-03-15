16d5c9d075905f63fc80f4d7e88ce79a29d1c796
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-03-14 19:14:47 -0600

    Created menu button functions

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void Solution()
+    {
+
+    }
+
+
+    public void AnimateSolution()
+    {
+
+    }
+
+
+    public void SelectPuzzle()
+    {
+
+    }
+}
