1ef587c5c773fbfdf52604f41d0660e5f92da22f
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-03-14 19:44:53 -0600

    Worked on button functionality

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -7,13 +7,16 @@ public class MenuScript : MonoBehaviour
 {
     public void Solution()
     {
-
+        //SceneManager.LoadScene("");
+        //instead of specifying scene name, one can do SceneManager.GetActiveScene().buildIndex + 1;
+        Debug.Log("Imagine solution here");
+        
     }
 
 
     public void AnimateSolution()
     {
-
+        Debug.Log("Imagine animated solution here");
     }
 
 
