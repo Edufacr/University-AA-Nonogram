69af718aeba5dc448e325aac146510335f3f2bd1
Eduardo <edumadrim@gmail.com>
2020-05-11 18:59:42 -0600

    rearrange gridScript

--- Assets/GridManager.cs
+++ Assets/GridManager.cs
@@ -7,73 +7,44 @@ using UnityEngine.Serialization;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private GameObject cellPrefab;
-    [SerializeField]
+    [SerializeField] private Vector2 gridSize;
     private int rows;
-    [SerializeField]
     private int cols;
-    [SerializeField]
-    private Vector2 gridSize;
-    [SerializeField]
     private Vector2 gridOffset;
-
-    //about cells
-    [SerializeField]
-    private Sprite cellSprite;
     private Vector2 cellSize;
-    private Vector2 cellScale;
-
+    
     void Start () {
-        InitCells(); //Initialize all cells
+        SetColRowNum(6,6);
+        InitializeCells();
+        InstantiateCells();
 	}
+
+    void SetColRowNum(int pRows, int pColumns)
+    {
+        rows = pRows;
+        cols = pColumns;
+    }
+    void InstantiateCells()
+    {
+        Vector2 transformPos = transform.position;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                Vector2 cellPos = new Vector2(col * cellSize.x + gridOffset.x + transformPos.x, row * cellSize.y + gridOffset.y + transformPos.y);
+                GameObject cell = Instantiate(cellPrefab, cellPos, Quaternion.identity);
+                cell.transform.parent = transform;
+            }
+        }
+    }
 
     void InitializeCells()
     {
-        
-    }
-    void InitCells() {
-        GameObject cellObject = cellPrefab;
-
-        //creates an empty object and adds a sprite renderer component -> set the sprite to cellSprite
-        //cellObject.AddComponent<SpriteRenderer>().sprite = cellSprite;
-        
-
-        //catch the size of the sprite
-        cellSize = cellObject.GetComponent<SpriteRenderer>().sprite.bounds.size;
-
-        //get the new cell size -> adjust the size of the cells to fit the size of the grid
+        cellSize = cellPrefab.GetComponent<SpriteRenderer>().sprite.bounds.size;
         Vector2 newCellSize = new Vector2(gridSize.x / (float)cols, gridSize.y / (float)rows);
-
-        //Get the scales so you can scale the cells and change their size to fit the grid
-        cellScale.x = newCellSize.x / cellSize.x;
-        cellScale.y = newCellSize.y / cellSize.y;
-
-        cellSize = newCellSize; //the size will be replaced by the new computed size, we just used cellSize for computing the scale
-
-        cellObject.transform.localScale = new Vector2(cellScale.x, cellScale.y);
-
-        //fix the cells to the grid by getting the half of the grid and cells add and minus experiment
+        cellPrefab.transform.localScale = new Vector2(newCellSize.x / cellSize.x,newCellSize.y / cellSize.y);
+        cellSize = newCellSize;
         gridOffset.x = -(gridSize.x / 2) + cellSize.x / 2;
         gridOffset.y = -(gridSize.y / 2) + cellSize.y / 2;
-
-        //fill the grid with cells by using Instantiate
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                //add the cell size so that no two cells will have the same x and y position
-                Vector2 pos = new Vector2(col * cellSize.x + gridOffset.x + transform.position.x, row * cellSize.y + gridOffset.y + transform.position.y);
-
-                //instantiate the game object, at position pos, with rotation set to identity
-                GameObject cO = Instantiate(cellObject, pos, Quaternion.identity) as GameObject;
-
-                //set the parent of the cell to GRID so you can move the cells together with the grid;
-                cO.transform.parent = transform;
-            }
-        }
-
-        //destroy the object used to instantiate the cells
-        Destroy(cellObject);
     }
-
-    //so you can see the width and height of the grid on editor
     void OnDrawGizmos() {
         Gizmos.DrawWireCube(transform.position, gridSize);
     }
