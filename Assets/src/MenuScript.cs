d70ee44f871364f37db4711e2792543e712edb3b
Eduardo <edumadrim@gmail.com>
2020-05-13 21:42:42 -0600

    Added boolean to know if a puzzle have been chosen (MenuScript) Added Painter foos to Menu Create foos in Painter to initialize, Paint single cell & Paint whole grid

--- Assets/src/MenuScript.cs
+++ Assets/src/MenuScript.cs
@@ -6,29 +6,37 @@ using UnityEditor;
 
 public class MenuScript : MonoBehaviour
 {
+    private bool puzzleSelected;
     private NonogramCreator creator;
     private NonogramSolver solver;
+    [SerializeField] private NonogramPainter painter;
 
     private void Start()
     {
         creator = NonogramCreator.GetInstance();
         solver = NonogramSolver.GetInstance();
+        puzzleSelected = false;
     }
 
     public void Solution()
     {
+        if (!puzzleSelected) return;
         solver.Solve(Nonogram.GetInstance(), false);
+        painter.PaintGrid(Nonogram.GetInstance());
     }
 
     public void AnimateSolution()
     {
-        Debug.Log("Imagine animated solution here");
-        solver.Solve(Nonogram.GetInstance(), true);
+        if (puzzleSelected)
+        {
+            solver.Solve(Nonogram.GetInstance(), true);
+        }
     }
 
     public void SelectPuzzle()
     {
         string path = EditorUtility.OpenFilePanel("", "", "txt");
         creator.CreateNonogram(path);
+        painter.InitializePainter(Nonogram.GetInstance());
     }
 }
--- Assets/src/NonogramPainter.cs
+++ Assets/src/NonogramPainter.cs
@@ -4,83 +4,36 @@ using UnityEngine;
 
 public class NonogramPainter : MonoBehaviour
 {
-
-    public GameObject squarePrefab;
-    public GameObject squareHolder; //Hay que pensar en el resto de la interfaz    
-    public float squareOffset; //offset entre cuadros
+    [SerializeField] private GridManager gridManager;
     
-    private int _columns;
-    private int _rows;
-    private Vector3 _squareSize;
-    private GameObject[,] _grid;
-    
-    public void SetGridSize(int pRows, int pColumns)
+    void Start()
     {
-        _rows = pRows;
-        _columns = pColumns;
-        _squareSize = GetSquareSize();
+    }
+    
+    public void InitializePainter(Nonogram pNonogram)
+    {
+        gridManager.InitializeGrid(pNonogram.Rows,pNonogram.Columns);
+    }
+    
+    public void PaintNonogram()
+    {
+        gridManager.CreateGrid();
     }
 
-    public void CreateGrid()
+    public void PaintGrid(Nonogram pNonogram)
     {
-        _grid = new GameObject[_rows,_columns];
-        for (int row = 0; row < _rows; row++)
+        int[,] matrix = pNonogram.Matrix;
+        for (int row = 0; row < pNonogram.Rows; row++)
         {
-            for (int column = 0; column < _columns; column++)
+            for (int column = 0; column < pNonogram.Columns; column++)
             {
-                _grid[row, column] = InstantiateSquare(row, column);
+                PaintGridCell(row,column,matrix[row,column]);
             }
         }
     }
 
-    private GameObject InstantiateSquare(int pRow, int pColumn)
+    public void PaintGridCell(int pRow, int pColumn,int pColorNum)
     {
-        GameObject square = Instantiate(squarePrefab, GetPosition(pRow,pColumn),Quaternion.identity);
-        //GameObject square = Instantiate(squarePrefab, squareHolder.transform);
-        //square.transform.parent = squareHolder.transform;
-        square.transform.localScale = _squareSize;
-        square.transform.name = "Square: " + pRow + "," + pColumn;
-
-        return square;
-    }
-
-    private Vector3 GetSquareSize()
-    {
-        float holderRectWidth = squareHolder.GetComponent<RectTransform>().rect.width;
-        float holderRectHeight = squareHolder.GetComponent<RectTransform>().rect.height;
-        return new Vector3(holderRectWidth/_columns,holderRectHeight/_rows);
-    }
-
-    private Vector3 GetPosition(int pRow, int pColumn)
-    {
-        float squareRadiusX = (squareHolder.GetComponent<RectTransform>().rect.width / _columns)/2;
-        float squareRadiusY = (squareHolder.GetComponent<RectTransform>().rect.height / _rows)/2;
-        Vector3[] holderCorners = new Vector3[4];
-        
-        float x = squareRadiusX * (1 + 2 * pColumn);
-        float y = squareRadiusY * (1 + 2 * pRow);
-        squareHolder.GetComponent<RectTransform>().GetWorldCorners(holderCorners);
-
-        Debug.Log("Radio X: "+squareRadiusX);
-        Debug.Log("Radio Y: "+squareRadiusY);
-        Debug.Log("X: "+x);
-        Debug.Log("Y: "+y);
-        Debug.Log(holderCorners[0].ToString());
-        return new Vector3(x,y)+holderCorners[0]; //0 es la esquina izquierda abajo debe cambiarse cuando se invierta la posicion (0,0)
-    }
-    
-    
-
-// Start is called before the first frame update
-    void Start()
-    {
-        SetGridSize(10,10);
-        CreateGrid();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        gridManager.ChangeCellSprite(pRow,pColumn,pColorNum);
     }
 }
