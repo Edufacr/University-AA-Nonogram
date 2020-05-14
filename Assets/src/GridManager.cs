4a7b0c78fe18e4cf2703962cf6b0340ed9c38134
Eduardo <edumadrim@gmail.com>
2020-05-13 21:39:56 -0600

    polished gridManager & added public functions

--- Assets/src/GridManager.cs
+++ Assets/src/GridManager.cs
@@ -14,39 +14,21 @@ public class GridManager : MonoBehaviour
     private int _columns;
     private Vector2 _gridOffset;
     private Vector2 _cellSize;
-    
     private GameObject[,] _grid;
-   
     
-    void Start () {
-        SetColRowNum(6,6);
+    public void InitializeGrid(int pRows, int pColumns)
+    {
+        SetColRowNum(pRows,pColumns);
         InitializeCells();
-        CreateGrid();
-        ChangeCellSprite(0,0,1);
-        ChangeCellSprite(2,2,1);
-        ChangeCellSprite(3,3,0);
-	}
-
-    void MirrorGrid()
-    {
-        for (int row = 0; row < _rows/2; row++)
-        {
-            int rowToSwap = (_rows - 1) - row;
-            for (int column = 0; column < _columns; column++)
-            {
-                SwapGridElements(row,column,rowToSwap,column);
-            }
-        }
     }
 
-    void SwapGridElements(int pRow1, int pColumn1, int pRow2, int pColumn2)
+    public void CreateGrid()
     {
-        GameObject temp = _grid[pRow1, pColumn1];
-        _grid[pRow1, pColumn1] = _grid[pRow2, pColumn2];
-        _grid[pRow2, pColumn2] = temp;
+        CreateBasicGrid();
+        MirrorGrid();
     }
-
-    void ChangeCellSprite(int pRow, int pColumn, int pColorNum)
+        
+    public void ChangeCellSprite(int pRow, int pColumn, int pColorNum)
     {
         SpriteRenderer spriteRenderer = _grid[pRow, pColumn].GetComponent<SpriteRenderer>();
         if (pColorNum == 1)
@@ -57,32 +39,19 @@ public class GridManager : MonoBehaviour
         {
             spriteRenderer.color = emptyCellColor;
         }
-            
     }
-    void SetColRowNum(int pRows, int pColumns)
+    
+    void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position, gridSize);
+    }
+    
+    private void SetColRowNum(int pRows, int pColumns)
     {
         _rows = pRows;
         _columns = pColumns;
     }
-    void CreateGrid()
-    {
-        _grid = new GameObject[_rows,_columns];
-        for (int row = 0; row < _rows; row++) {
-            for (int column = 0; column < _columns; column++) {
-                _grid[row, column] = InstantiateCell(row, column,transform.position);
-            }
-        }
-        MirrorGrid();
-    }
-    GameObject InstantiateCell(int pRow, int pColumn, Vector2 pBasePosition)
-    {
-        Vector2 cellPos = new Vector2(pColumn * _cellSize.x + _gridOffset.x + pBasePosition.x, pRow * _cellSize.y + _gridOffset.y + pBasePosition.y);
-        GameObject cell = Instantiate(cellPrefab, cellPos, Quaternion.identity);
-        cell.transform.parent = transform;
-        cell.transform.name = "Cell: " + pRow + "," + pColumn;
-        return cell;
-    }
-    void InitializeCells()
+    
+    private void InitializeCells()
     {
         _cellSize = cellPrefab.GetComponent<SpriteRenderer>().sprite.bounds.size;
         Vector2 newCellSize = new Vector2(gridSize.x / (float)_columns, gridSize.y / (float)_rows);
@@ -91,7 +60,42 @@ public class GridManager : MonoBehaviour
         _gridOffset.x = -(gridSize.x / 2) + _cellSize.x / 2;
         _gridOffset.y = -(gridSize.y / 2) + _cellSize.y / 2;
     }
-    void OnDrawGizmos() {
-        Gizmos.DrawWireCube(transform.position, gridSize);
+    
+    private void CreateBasicGrid()
+    {
+        _grid = new GameObject[_rows,_columns];
+        for (int row = 0; row < _rows; row++) {
+            for (int column = 0; column < _columns; column++) {
+                _grid[row, column] = InstantiateCell(row, column,transform.position);
+            }
+        }
+    }
+    
+    private GameObject InstantiateCell(int pRow, int pColumn, Vector2 pBasePosition)
+    {
+        Vector2 cellPos = new Vector2(pColumn * _cellSize.x + _gridOffset.x + pBasePosition.x, pRow * _cellSize.y + _gridOffset.y + pBasePosition.y);
+        GameObject cell = Instantiate(cellPrefab, cellPos, Quaternion.identity);
+        cell.transform.parent = transform;
+        cell.transform.name = "Cell: " + pRow + "," + pColumn;
+        return cell;
+    }
+    
+    private void MirrorGrid()
+    {
+        for (int row = 0; row < _rows/2; row++)
+        {
+            int rowToSwap = (_rows - 1) - row;
+            for (int column = 0; column < _columns; column++)
+            {
+                SwapGridElements(row,column,rowToSwap,column);
+            }
+        }
+    }
+    
+    private void SwapGridElements(int pRow1, int pColumn1, int pRow2, int pColumn2)
+    {
+        GameObject temp = _grid[pRow1, pColumn1];
+        _grid[pRow1, pColumn1] = _grid[pRow2, pColumn2];
+        _grid[pRow2, pColumn2] = temp;
     }
 }
